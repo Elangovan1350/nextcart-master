8f960a770e71752b4705013aa69d7324cf1ae408
Elangovan1350 <elangovan2019miss@gmail.com>
2026-01-24 14:55:30 +0530

    change6

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(
-  request: NextResponse,
+  request: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
